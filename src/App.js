665d9977c5a82d285e2dd82fdfc10da96fb1f049
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-12-08 22:27:29 -0800

    added component header and folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import HomePage from "./Page/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/:videoId" element={<HomePage />} />
+        <Route path="/upload" element={<Upload />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,49 @@
+import "./Header.scss";
+import logo from "../../assets/icons/Logo1.png";
+import icon from "../../assets/icons/icon8.png";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+  const [openlist, setOpenList] = useState(false);
+
+  return (
+    <section className="header">
+      <div className="header__container">
+        <div className="header__logo-container">
+          <img className="header__logo" src={logo} alt="Company logo" />
+        </div>
+        <div className="header__hamburger">
+          <img
+            className="header__hamburger-icon"
+            src={icon}
+            alt="Company logo"
+            onClick={() => {
+              setOpenList(!openlist);
+            }}
+          />
+        </div>
+
+        <div className="header__right-container">
+          <div className="header__list">Packages</div>
+          <div className="header__list">How it Works</div>
+          <div className="header__list">About</div>
+          <div className="header__list">Contact Us</div>
+          <div className="header__list">Cart</div>
+        </div>
+      </div>
+      {openlist === true ? (
+        <div className="header__hamburger-list-container">
+          <div className="header__hamburger-list">
+            <div className="header__hamburger-item">Packages</div>
+            <div className="header__hamburger-item">How it Works</div>
+            <div className="header__hamburger-item">About</div>
+            <div className="header__hamburger-item">Contact Us</div>
+            <div className="header__hamburger-item">Cart</div>
+          </div>
+        </div>
+      ) : (
+        " "
+      )}
+    </section>
+  );
+}
