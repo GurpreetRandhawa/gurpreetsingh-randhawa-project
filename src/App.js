c5c32dc48eb14d0c285b6c42e1138f62871ec5f8
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-12-10 13:04:22 -0800

    add main component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Main from "./components/main/Main";
 import HomePage from "./Page/HomePage/HomePage";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Hero />
+      <Main />
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* <Route path="/:videoId" element={<HomePage />} />
